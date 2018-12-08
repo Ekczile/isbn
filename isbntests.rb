337a7e2ea29e4e0ce7f2959a04dbf83cbc794a81
Steven <ekczile@gmail.com>
2018-12-08 02:41:38 -0500

    adding tests

--- isbntests.rb
+++ isbntests.rb
@@ -14,6 +14,12 @@ class Testing < Minitest::Test
     def test_valids
         assert_equal("Valid", isbn10("0-330-28498-3"))
         assert_equal("Valid", isbn10("0-321-14653-0"))
+        assert_equal("Valid", isbn10("2-226-05257-7"))
+        assert_equal("Valid", isbn10("3-7965-1900-8"))
+        assert_equal("Valid", isbn10("5-85270-001-0"))
+        assert_equal("Valid", isbn10("5-85270-001-0"))
+        assert_equal("Valid", isbn10("605-384-057-2"))
+        assert_equal("Valid", isbn10("609-01-1248-8"))
         assert_equal("Valid", isbn10("877195869x"))
     end
 
