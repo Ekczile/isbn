1bd65c66d0fed36c1c3607c79ff0f738a35be983
Steven <ekczile@gmail.com>
2018-12-08 02:36:30 -0500

    finished isbn10 function

--- isbn.rb
+++ isbn.rb
@@ -1,5 +1,22 @@
 def isbn10(numbers)
-    numbers = numbers.to_s
-    numbers = numbers.gsub("-", "").each_char.to_a
-    p numbers
+    counter = 1 #counter
+    final = Array.new #new array
+    checknumber = Array.new #new array
+    ary = Array.new #new array
+    numbers = numbers.gsub("-", "").gsub(" ", "").each_char.to_a #removing - and white spaces
+     numbers.each do |v| #loop 
+       ary.push(v.gsub("x", "10").to_i) #pushing each char into new array as an integer and if v is x convert x to 10
+     end
+     checknumber.push(ary[9]) #storing last number to check later
+     ary.delete_at(9) # array with 10th digit removed
+     ary.each do |v| #loop
+          final.push(v * counter) #push each digit times the counter into final array
+          counter += 1 #counter goes up with each index 1,2,3,4,5,6,7,8,9 respectivly
+     end
+    final = final.sum%11 #sum of all indexs modul 11
+    if final == checknumber[0] # conditional for if final is equal to the stored digit
+      p "Valid"
+    else
+      p "Invalid"
+    end
 end
--- isbntests.rb
+++ isbntests.rb
@@ -7,7 +7,19 @@ class Testing < Minitest::Test
         assert_equal(1,1)
     end
     
-    def test_2
-        assert_equal(Array, isbn10("0-330-28498-3").class)
+    # def test_2
+    #     assert_equal(Array, isbn10("0-330-28498-3").class)
+    # end
+
+    def test_valids
+        assert_equal("Valid", isbn10("0-330-28498-3"))
+        assert_equal("Valid", isbn10("0-321-14653-0"))
+        assert_equal("Valid", isbn10("877195869x"))
+    end
+
+    def test_invalid
+        assert_equal("Invalid", isbn10("978 0 471 48648 0"))
+        assert_equal("Invalid", isbn10("0-321@14653-0"))
+        assert_equal("Invalid", isbn10("877195x869"))
     end
 end
