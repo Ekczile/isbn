64c4344f3f47d9b145fd0141a3b21d6102c0cbde
Steven <ekczile@gmail.com>
2018-12-12 00:35:00 -0500

    fixed logic on validation page

--- app.rb
+++ app.rb
@@ -15,19 +15,26 @@ end
 get '/home' do
     tencheck = params[:ten]
     threecheck = params[:thirteen]
+    p "this is params in home #{params}"
     erb :home, locals: {tencheck: tencheck, threecheck: threecheck}
 end
 
 post '/home' do
+    tencheck = params[:ten]
+    threecheck = params[:thirteen]
     params[:isbn10] ? avalidation = params[:isbn10] : avalidation = ""
     params[:isbn13] ? bvalidation = params[:isbn13] : bvalidation = ""
-    redirect 'validation?isbn10=' + avalidation + "&isbn13=" + bvalidation
+    p "this is params in home post #{params}"
+    redirect 'validation?isbn10=' + avalidation + "&isbn13=" + bvalidation + "&ten=" + tencheck + "&thirteen=" + threecheck
 end
 
 get "/validation" do
+    tencheck = params[:ten]
+    threecheck = params[:thirteen]
     avalidation = params[:isbn10]
     bvalidation = params[:isbn13]
     isbn10 = isbn10(avalidation)
     isbn13 = isbn13(bvalidation)
-    erb :validation, locals: {isbn10: isbn10, isbn13: isbn13}
+    p "this is params in val #{params}"
+    erb :validation, locals: {isbn10: isbn10, isbn13: isbn13, tencheck: tencheck, threecheck: threecheck}
 end
