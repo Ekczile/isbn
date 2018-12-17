786cdefa01244600f0e2d7ad277d313e691d56fa
Steven <ekczile@gmail.com>
2018-12-16 20:55:18 -0500

    single input box as requested by marv

--- app.rb
+++ app.rb
@@ -8,42 +8,40 @@ get '/' do
 end
 
 post '/landing' do
-    params[:thirteen] ? threecheck = params[:thirteen] : threecheck = ""
-    params[:ten] ? tencheck = params[:ten] : tencheck = ""
+    params[:threeten] ? isbncheck = params[:threeten] : isbncheck = ""
     params[:csvfile] ? csvcheck = params[:csvfile] : csvcheck = ""
-    redirect 'home?ten=' + tencheck + '&thirteen=' + threecheck + '&csvfile=' + csvcheck
+    redirect 'home?threeten=' + isbncheck + '&csvfile=' + csvcheck
 end
 
 get '/home' do
-    tencheck = params[:ten]
+    isbncheck = params[:threeten]
     threecheck = params[:thirteen]
     csvcheck = params[:csvfile]
-    erb :home, locals: {tencheck: tencheck, threecheck: threecheck, csvcheck: csvcheck}
+    erb :home, locals: {isbncheck: isbncheck, csvcheck: csvcheck}
 end
 
 post '/home' do
-    tencheck = params[:ten]
-    threecheck = params[:thirteen]
+    isbncheck = params[:threeten]
     csvfile = params[:csvfile]
-    params[:isbn10] ? avalidation = params[:isbn10] : avalidation = ""
-    params[:isbn13] ? bvalidation = params[:isbn13] : bvalidation = ""
+    params[:isbn] ? avalidation = params[:isbn] : avalidation = ""
     params[:myFile] ? csvval = params[:myFile] : csvval = ""
-    redirect 'validation?isbn10=' + avalidation + "&isbn13=" + bvalidation + "&ten=" + tencheck + "&thirteen=" + threecheck + '&csvfile=' + csvfile + '&myFile=' + csvval
+    redirect 'validation?isbn=' + avalidation + "&threeten=" + isbncheck + '&csvfile=' + csvfile + '&myFile=' + csvval
 end
 
 get "/validation" do
-    tencheck = params[:ten]
-    threecheck = params[:thirteen]
+    isbncheck = params[:threeten]
     csvfile = params[:csvfile]
-    avalidation = params[:isbn10]
-    bvalidation = params[:isbn13]
+    avalidation = params[:isbn].tr("a-w", "").tr("y-z", "").gsub("-", "").gsub(" ", "")
+    if avalidation.length == 10
+        avalidation = avalidation + " is " + isbn10(avalidation)
+    else
+        avalidation = avalidation + " is " + isbn13(avalidation)
+    end
     csvval = params[:myFile]
-    isbn10 = isbn10(avalidation)
-    isbn13 = isbn13(bvalidation)
     if csvval != ""
         validated = makefile(csvval)
     else
         validated = ""
     end
-    erb :validation, locals: {isbn10: isbn10, isbn13: isbn13, tencheck: tencheck, threecheck: threecheck, avalidation: avalidation, bvalidation: bvalidation, csvfile: csvfile, csvval: csvval, validated: validated}
+    erb :validation, locals: {isbncheck: isbncheck, avalidation: avalidation, csvfile: csvfile, csvval: csvval, validated: validated}
 end
